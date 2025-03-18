394369028aee8ac762b6ec56c307202ff6db8737
ramonbezerra <ramon.bezerra@outlook.com>
2025-03-18 19:26:43 -0300

    rename aula 12

---
+++ notification_example.py
@@ -0,0 +1,8 @@
+from plyer import notification
+
+notification.notify(
+    title = 'Exemplo de Notificacao',
+    message = 'Messagem de exemplo',
+    timeout = 10,
+    app_name = 'App de exemplo'
+)
